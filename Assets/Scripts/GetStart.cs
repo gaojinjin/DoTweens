dc2894a97a4063ab2445f31032721415304efb89
gaojinjin <>
2018-08-24 21:59:22 +0800

    初级脚本

---
+++ Assets/Scripts/GetStart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class GetStart : MonoBehaviour {
+
+    public Vector3 tempVector = Vector3.zero;
+    // Use this for initialization
+    public RectTransform imagePos;
+    private bool isIn = false;
+	void Start () {
+        DOTween.To(()=>tempVector,x=> tempVector =x,new Vector3(10,10,10),20f);
+        // imagePos.DOMove(new Vector3(0, 0, 0), 10f);//修改的是世界坐标
+        Tween tweener = imagePos.DOLocalMove(new Vector3(0, 0, 0), 10f);//默认动画播放完成后会被销毁
+                                                                        //Tweener对象保存这个动画的信息，每次掉用do类型方法都会创建一个tweener，这个对像由dotween管理
+        tweener.SetAutoKill(false);
+        tweener.Pause();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = tempVector;
+	}
+
+    public void OnClick(){
+        if (isIn == false)
+        {
+            //Doplay  掉用一次。  多次掉用使用DoPlayForward
+            imagePos.DOPlayForward();
+            isIn = true;
+           
+        }
+        else
+        {
+            isIn = false;
+            imagePos.DOPlayBackwards();
+        }
+
+    }
+}
---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class MoveCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //transform.DOMoveX(6, 1);
+        //默认从指定位置运行到当前位置
+        //transform.DOMoveX(6, 1).From();
+        //这种情况 是从目标位置+6的X位置移动过来~
+        transform.DOMoveX(6, 1).From(true);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
